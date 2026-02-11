4edc179cba4007a0e4b324c60b8c991bfbec8bde
BudenchukM <budenchuk.marina@gmail.com>
2026-02-11 12:10:15 +0300

    token

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,8 +1,7 @@
-import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-import { authActions, getStoredUser } from '../slices/auth'
+import { authActions } from '../slices/auth'
 import { useLoginMutation } from '../api/chatApi'
 import { pages } from '../utils/routes'
 
@@ -16,46 +15,28 @@ export const useAuth = () => {
     try {
       const user = await loginMutation(values).unwrap()
 
-      localStorage.setItem('user', JSON.stringify(user))
       dispatch(authActions.setAuth(user))
-
       navigate(pages.root())
     }
     catch (err) {
-      // ❗ ВАЖНО: показываем Formik-ошибку
       if (err?.status === 401) {
         formikHelpers.setStatus(401)
         formikHelpers.setSubmitting(false)
         return
       }
 
-      // любые другие ошибки просто логируем
       console.error(err)
     }
   }
 
   // ---------- LOGOUT ----------
   const logout = () => {
-    localStorage.removeItem('user')
     dispatch(authActions.removeAuth())
     navigate(pages.login())
   }
 
-  // ---------- INIT AUTH ----------
-  const initAuth = () => {
-    const user = getStoredUser()
-    if (user) {
-      dispatch(authActions.setAuth(user))
-    }
-  }
-
-  useEffect(() => {
-    initAuth()
-  }, [])
-
   return {
     login,
     logout,
-    initAuth,
   }
 }
