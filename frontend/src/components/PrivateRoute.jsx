b7a2c3d4b2ed2c6990b7f05ad51498b765c5bba2
BudenchukM <budenchuk.marina@gmail.com>
2026-02-11 12:15:00 +0300

    token

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,14 +1,13 @@
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
-import { selectAuth, getStoredUser } from '../slices/auth'
+import { selectAuth } from '../slices/auth'
 import { pages } from '../utils/routes'
 
 const PrivateRoute = ({ children }) => {
   const auth = useSelector(selectAuth)
-  const userAuthInfo = getStoredUser()
 
-  if (!auth.token && !userAuthInfo) {
-    return <Navigate to={pages.login()} replace />
+  if (!auth.token) {
+    return <Navigate to={pages.login()} />
   }
 
   return children
